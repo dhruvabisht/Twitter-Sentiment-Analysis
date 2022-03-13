236b890986731818a7a051cc931d63c6430588bd
dhruvabisht <dhruvabisht.18@gmail.com>
2022-03-13 18:32:28 +0530

    This is the first commit for the sentiment analysis project. All required libraries have been imported successfully

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,12 @@
+#CREATING THE IMPORTS AND GETTING ALL REQUIRED LIBRARIES
+
+from tkinter import *
+import tweepy
+from textblob import TextBlob
+import pandas as pd
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+#NOW INITIALISING THE ROOT VARIABLE FOR THE GUI LOOP
+root = Tk()
